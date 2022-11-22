ed20e4e21834c7aaf30d405fc5e89a9d0e64d7a9
Claire <clairose0@gmail.com>
2022-11-21 19:41:44 -0700

    god bless america """"technically playable""""

--- graphics.py
+++ graphics.py
@@ -1,12 +1,14 @@
 import turtle
 
+# Preliminaries
 window = turtle.Screen()
-window.setup(648,648)
+window.setup(648,648) # (6**4)/2 so it is "small" but easily divisible by 2 and 3
 window.bgcolor("white")
-window.title("Turtle")
+window.title("Ultimate Ultimate Tik Tac Toe")
 t = turtle.Turtle()
-t.speed(10)
+t.speed(0)
 
+# basically defines the parameters of a tic tac toe board
 def board(tlCornerX, tlCornerY, length, size):
     t.ht()
     t.width(size)
@@ -28,6 +30,8 @@ def board(tlCornerX, tlCornerY, length, size):
         t.down()
         t.forward(length)
     t.left(90)
+
+# small boards
 tlCornerX = -324
 tlCornerY = 324
 length = 72
@@ -47,6 +51,7 @@ for i in range(9):
     else:
         tlCornerX -= length
 
+# medium boards (i can't be bothered putting this in a for loop because it would be more lines. sue me)
 board(108, -108, 216, 3)
 board(-108, -108, 216, 3)
 board(-324, -108, 216, 3)
@@ -56,5 +61,31 @@ board(108, 108, 216, 3)
 board(108, 324, 216, 3)
 board(-108, 324, 216, 3)
 board(-324, 324, 216, 3)
+
+# big board
 board(-324, 324, 648, 5)
-turtle.done()
+
+b = turtle.Turtle()
+b.speed(0)
+b.up()
+b.shape('square')
+b.shapesize(288, 288)
+b.fillcolor('')
+
+boo = True
+
+def click(x, y):
+    global boo
+    if boo:
+        b.goto(x + 1, y - 15)
+        b.write("o", align="center", font=("Arial", 30, "bold"))
+        boo = False
+    else:
+        b.goto(x + 1, y - 15)
+        b.write("x", align="center", font=("Arial", 30, "bold"))
+        boo = True
+        
+
+b.onclick(click)
+
+turtle.mainloop()
